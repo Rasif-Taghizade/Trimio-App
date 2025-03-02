122b272139a60da420e45f4b89e9dc8f34755165
Aysun <aysuntaghizade@gmail.com>
2025-03-02 18:26:01 +0400

    [TR-6]: Add all colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,13 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    light100: '#FDFDFD',
+    light200: '#FBFBFB',
+    light300: '#F8F8F8',
+    light400: '#F6F6F6'
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
+    dark100: '#A2A6B1',
+    dark200: '#797D8A',
+    dark300: '#6C707D'
   },
 };
